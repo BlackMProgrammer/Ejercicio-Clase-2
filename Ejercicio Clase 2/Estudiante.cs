b6f697888f6dbfb0b61462f12d0bcec5c168eacd
BlackMProgrammer <cristhiancastro01@gmail.com>
2020-09-14 14:36:23 -0400

    Add project files.

---
+++ Ejercicio Clase 2/Estudiante.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Ejercicio_Clase_2
+{
+    class Estudiante
+    {
+        private string _nombre;
+        private int _nota1;
+        private int _nota2;
+        private int _nota3;
+        private int _promedio;
+
+        public Estudiante(string _nombre, int _nota1, int _nota2, int _nota3) 
+        {
+            this._nombre = _nombre;
+            this._nota1 = _nota1;
+            this._nota2 = _nota2;
+            this._nota3 = _nota3;
+            if (_nota1 > 100 || _nota2 > 100 || _nota3 > 100)
+            {
+                Console.WriteLine("La nota no puede ser mayor a 100");
+            }
+        }
+
+        public int promedio() {
+
+            _promedio = (_nota1 + _nota2 + _nota3) / 3;
+            Console.WriteLine("Su promedio es de :{0}",_promedio);
+            return _promedio;
+        }
+    }
+}
---
+++ Ejercicio Clase 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ejercicio_Clase_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el nombre del estudiante");
+
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Digite la primera nota: ");
+            string nota1val = Console.ReadLine();
+            int nota1 = Convert.ToInt32(nota1val);
+
+            Console.WriteLine("Digite la segunda nota: ");
+            string nota2val = Console.ReadLine();
+            int nota2 = Convert.ToInt32(nota2val);
+
+            Console.WriteLine("Digite la tercena nota: ");
+            string nota3val = Console.ReadLine();
+            int nota3 = Convert.ToInt32(nota3val);
+
+            Estudiante estudiante = new Estudiante(nombre,nota1,nota2,nota3);
+            estudiante.promedio();
+            
+        }
+    }
+}
